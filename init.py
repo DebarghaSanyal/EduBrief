71e633381834c675030099efa9f33b12f44dc970
DebarghaSanyal <debarghasanyal128@gmail.com>
2025-03-11 22:20:37 +0530

    Added model configuration & requirements

---
+++ init.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+import nltk
+import google.generativeai as genai
+import assemblyai as aai
+
+# Load environment variables
+load_dotenv()
+ASSEMBLYAI_API_KEY = os.getenv("ASSEMBLYAI_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# Configure APIs
+aai.settings.api_key = ASSEMBLYAI_API_KEY
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")   # gentai model name (Must be free version)
+
+# Download NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('punkt_tab')  # zip
+
+print("Initialization complete.")
